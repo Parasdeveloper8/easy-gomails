7df27b14bad94aa9b50ba99546c13e59f3b34ef6
Paras prajapat <parasprajapat8th.gmail.com>
2024-11-28 21:31:13 +0530

    added .gitignore

---
+++ testing/main.go
@@ -0,0 +1,7 @@
+package main
+
+import easymails "github.com/Parasdeveloper8/easy-gomails.git/mails"
+
+func main() {
+	easymails.SendMail("parasprajapat8th@gmail.com", "qubo apkh tycc clgv", "shaluparas95@gmail.com", "Testing package", "hello")
+}
